688365a9e0021fa23909d0aab89affbf76765e33
cody-mick <cmick@byui.edu>
2022-02-19 11:40:16 -0700

    removed secret key from public view

--- millionaire/millionaire/settings.py
+++ millionaire/millionaire/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&o-^=smgkfyi2n14!6&3)#@2g78y#u)d_@!v=+)t5s-omcr9&*'
+# SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
