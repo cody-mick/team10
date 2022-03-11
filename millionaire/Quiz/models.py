d196832a5060b1f742c6a6163814358b25d3bfc2
cody-mick <cmick@byui.edu>
2022-03-11 13:27:42 -0700

    bringing it all up to speed

--- millionaire/Quiz/models.py
+++ millionaire/Quiz/models.py
@@ -8,6 +8,8 @@ class QuesModel(models.Model):
     op3 = models.CharField(max_length=200,null=True)
     op4 = models.CharField(max_length=200,null=True)
     ans = models.CharField(max_length=200,null=True)
+    hint = models.CharField(max_length=400, null=True)
+    point_value = models.IntegerField(max_length=7, null=False, default=0)
     
     def __str__(self):
         return self.question
